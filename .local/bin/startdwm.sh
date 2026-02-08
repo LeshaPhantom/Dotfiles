fff7a41f428af6f142523c4f36aa89658b94c82a
Иликаев Алексей Леонидович <a.l.ilikaev@tinkoff.ru>
2026-02-08 10:39:52 +0300

    Удалил для нового Дотфайлов

--- .local/bin/startdwm.sh
+++
@@ -1,9 +0,0 @@
-# /bin/sh
-
-setxkbmap -layout "us,ru" -option "grp:caps_toggle"
-/home/lesha/.local/bin/newlook
-xcompmgr &
-
-while true; do 
-	dwm 2> ~/.dwm.log
-done
--- .zprofile
+++
@@ -1,3 +0,0 @@
-
-# Created by `pipx` on 2024-08-29 15:30:47
-export PATH="$PATH:/home/lesha/.local/bin"
